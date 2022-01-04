9f885254d64d26d8e1f3b16b0079468e524ea3c6
Igor Miranda <igorpcmiranda@gmail.com>
2022-01-04 20:32:20 -0300

    Resolvido conflito de merges

--- src/batalha/Jogo.java
+++ src/batalha/Jogo.java
@@ -1,10 +1,8 @@
 package batalha;
 
 public class Jogo {
-<<<<<<< HEAD
-	
-	
-	
+
+
 	public void jogarBombaNoComputador() {
 		
 	}
@@ -13,11 +11,7 @@ public class Jogo {
 		
 	}
 	
-	
-=======
 
-
->>>>>>> a0d82e802b5ff292c40abcec5c91ff9f06505038
     public static void main(String[] args) {
         Tabuleiro tab = new Tabuleiro();
         Usuario usuario = new Usuario();
@@ -25,11 +19,8 @@ public class Jogo {
         
         tab.startMatrix();
         tab.showMatrix();
-<<<<<<< HEAD
         usuario.posicionarNavios();
-        
-        
-        
+    
        // public void jogar(){
        // 	while ((user.points || computer.points) < 10){
        //		jogarBombaNoComputador()
@@ -38,9 +29,8 @@ public class Jogo {
        // }
    
         
-=======
         tab.addShipMatrix(1,3);
         tab.showMatrix();
->>>>>>> a0d82e802b5ff292c40abcec5c91ff9f06505038
+
     }
 }

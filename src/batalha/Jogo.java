71c1e6632e3b72534acd4f46257f8a8a9e47e0ec
Igor Miranda <igorpcmiranda@gmail.com>
2022-01-03 21:48:49 -0300

    Adicionado instancia de usuario + esboço do método Jogar()

--- src/batalha/Jogo.java
+++ src/batalha/Jogo.java
@@ -1,10 +1,32 @@
 package batalha;
 
 public class Jogo {
+	
+	
+	
+	public void jogarBombaNoComputador() {
+		
+	}
+	
+	
     public static void main(String[] args) {
         Tabuleiro tab = new Tabuleiro();
-
+        Usuario usuario = new Usuario();
+        
+        
         tab.startMatrix();
         tab.showMatrix();
+        usuario.posicionarNavios();
+        
+        
+        
+       // public void jogar(){
+       // 	while ((user.points || computer.points) < 10){
+       //		jogarBombaUser()
+       //       jogarBombaComputer()
+       // }
+       // }
+   
+        
     }
 }

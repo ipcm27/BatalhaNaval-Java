74b87c778691196a07ad029ecd3f70967e83c205
Bentroen <bemcdc@gmail.com>
2022-01-07 21:15:10 -0300

    Adicionar lógica do jogo na classe Jogo

--- src/batalha/Jogo.java
+++ src/batalha/Jogo.java
@@ -2,35 +2,36 @@ package batalha;
 
 public class Jogo {
 
-
-	public void jogarBombaNoComputador() {
-		
-	}
-	
-	public void jogarBombaNoUsuario() {
-
-	}
-
-
     public static void main(String[] args) {
-        Tabuleiro tabIgor = new Tabuleiro();
-        Tabuleiro tabBer = new Tabuleiro();
-        
-        Usuario igor = new Usuario(tabIgor);
-        Computador bernado = new Computador(tabBer);
 
-        
-        igor.posicionarNavios();
-        bernado.posicionarNaviosComputador(igor,bernado);
-        bernado.shootComputer(tabIgor);
-        tabIgor.showMatrix();
-        tabBer.showMatrixComputador();
-    
-       // public void jogar(){
-       // 	while ((user.points || computer.points) < 10){
-       //		jogarBombaNoComputador()
-       //       jogarBombaNoUsuario()
-       // }
-       // }
+        Tabuleiro tabUser = new Tabuleiro();
+        Tabuleiro tabComputer = new Tabuleiro();
+
+        Usuario user = new Usuario(tabUser);
+        Computador computer = new Computador(tabComputer);
+
+        user.posicionarNavios();
+        computer.posicionarNaviosComputador(user, computer);
+
+        jogar(user, computer);
+
+        tabUser.showMatrix();
+        tabComputer.showMatrixComputador();
+
     }
+
+    private static void jogar(Usuario user, Computador computer) {
+        while (true) {
+            user.atacarComputador(computer.tabuleiroComputador);
+            computer.atacarUsuario(user.tabuleiroUsuario);
+            if (user.getPoints() >= 10) {
+                System.out.printf("Você venceu!");
+                break;
+            } else if (computer.getPoints() >= 10) {
+                System.out.printf("Você perdeu!");
+                break;
+            }
+        }
+    }
+
 }
